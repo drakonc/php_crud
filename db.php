b11cea76b550e87c81dcd415bfec605ac0cde473
Jose Castro Cantillo <drakonc@gmail.com>
2019-12-28 21:26:11 -0500

    first commit

---
+++ db.php
@@ -0,0 +1,3 @@
+<?php 
+    $conn = mysqli_connect('localhost','root','Passw0rd!!','php_mysql_crud');
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("db.php") ?>
+<?php include("includes/header.php")?>
+
+<div class="container mt-5">
+    <h1>Hola Mundo</h1>
+</div>
+
+<?php include("includes/footer.php")?>
+
